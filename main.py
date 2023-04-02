c4c8e85bc446dbbd4c22cf47d50107a506494f1a
Vince <vince@surfin.sg>
2023-04-02 12:04:19 +0800

    initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+"""
+main entry point to the application
+"""
+from langchain.llms import OpenAI
+
+llm = OpenAI(temperature=0.9)
+text = "What would be a good company name for a company that makes colorful socks?"
+print(llm(text))
