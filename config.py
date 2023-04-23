fab66a39dbee6d853d381535cdbb75effa5341b0
Vince Lam <vince@surfin.sg>
2023-04-23 15:59:47 +0800

    fix logging level config

--- config.py
+++ config.py
@@ -1,16 +1,13 @@
+"""
+setup the configuration from config.yaml
+"""
 import logging
-import os
 import sys
 from pathlib import Path
 from pprint import pformat
 
 import yaml
 
-logging.basicConfig(
-    level=logging.DEBUG,
-    format="%(asctime)s.%(msecs)03d [%(levelname)s] %(filename)s %(message)s %(pathname)s:%(lineno)d",
-    handlers=[logging.FileHandler("debug.log"), logging.StreamHandler(sys.stdout)],
-)
 # Load config items from config.yaml.
 # Use Path.resolve() to get the absolute path of the parent directory
 yaml_dir = Path(__file__).resolve().parent
@@ -20,26 +17,28 @@ yaml_path = yaml_dir / "config.yaml"  # Use Path / operator to join paths
 def load_yaml_config(path):
     """Load a yaml file and return a dictionary of its contents."""
     try:
-        with open(path, "r") as stream:
+        with open(path, "r", encoding="utf-8") as stream:
             return yaml.safe_load(stream)
     except yaml.YAMLError as exc:
-        logging.error(f"Failed to load {path}: {exc}")
+        logging.exception(exc)
         return None
 
 
 # Load the config and update the global variables
 yaml_config = load_yaml_config(yaml_path)
 if yaml_config is not None:
-    logging.info(f"Loaded config from {yaml_path}:")
+    logging.info("Loaded config from %s:", yaml_path)
     logging.info(pformat(yaml_config))
     globals().update(yaml_config)
+    logging.basicConfig(
+        level=logging.DEBUG if SANIC.get("DEBUG", True) else logging.INFO,
+        format="%(asctime)s.%(msecs)03d [%(levelname)s]"
+        + " %(filename)s %(message)s %(pathname)s:%(lineno)d",
+        handlers=[
+            logging.FileHandler("debug.log"),
+            logging.StreamHandler(sys.stdout),
+        ],
+    )
 else:
-    logging.error(f"Could not load config from {yaml_path}.")
+    logging.error("Could not load config from %s.", yaml_path)
     sys.exit(1)  # Exit the program if the config is invalid
-
-
-# Set a default value for SERVER_PORT if not specified in the config
-# SERVER_PORT = yaml_config.get("SERVER_PORT", None)
-
-# Use Path.resolve() to get the absolute path of the current directory
-# SERVER_DIR = Path(__file__).resolve().parent
