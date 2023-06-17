f80881882872729a09c6a7c6814bac1a4bd745e6
Vince Lam <vince@surfin.sg>
2023-06-17 13:45:11 +0800

    change limit to lower

--- kbgpt/svc/utils.py
+++ kbgpt/svc/utils.py
@@ -29,8 +29,8 @@ MODEL_COST_PER_1K_TOKENS = {
 }
 
 MODEL_LIMIT_PER_MINUTE = {
-    "gpt-3.5-turbo" : 900,
-    "gpt-3.5-turbo-0301": 900
+    "gpt-3.5-turbo" : 90000 - 40000,
+    "gpt-3.5-turbo-0301": 90000 - 40000
 }
 
 
