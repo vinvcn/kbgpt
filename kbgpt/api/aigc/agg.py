c194383f60a7ceb86da319d321dd8174b96d3ae6
Vince Lam <vince@surfin.sg>
2023-09-21 12:01:36 +0800

    update

--- kbgpt/api/aigc/agg.py
+++ kbgpt/api/aigc/agg.py
@@ -17,7 +17,7 @@ from kbgpt.api.constants import API_CONTENT_TYPE
 from kbgpt.api.libs.base_model import ErrorResponse
 from kbgpt.api.libs.utils import jtext
 from kbgpt.lib.db.vector_store import BusinessType, create_vector_store_strategy
-from kbgpt.lib.llm.multi_client import CLIENT
+from kbgpt.lib.llm.multi_client import CLIENT, AllClientRateExceedError, Completion
 from kbgpt.lib.llm.openai import Message, client
 from kbgpt.svc.aigc.qa.qa_services import QAagent
 
@@ -51,12 +51,18 @@ async def gen_prompt(
     kwargs.pop("docs", "")
     kwargs.pop("temperature", "")
     if tname == "recom_by_conversation.txt":
-        result = await CLIENT.chat_completion(
-            messages=[Message(role="system", content=prompt)],
-            stream=stream,
-            temperature=0.0,
-            **kwargs,
-        )
+        try:
+            result = await CLIENT.chat_completion(
+                messages=[Message(role="system", content=prompt)],
+                stream=stream,
+                temperature=0.0,
+                **kwargs,
+            )
+        except AllClientRateExceedError as e:
+            result = Completion(
+                prompt=prompt,
+                content="Sorry, we are currently having an issue, please try again later.",
+            )
     else:
         result = await openai.chat_completion(
             gpt_model,
